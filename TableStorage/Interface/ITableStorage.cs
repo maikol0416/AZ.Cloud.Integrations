1ea64818d4c5fbc77e9644186cf59bb916456785
maikol0416 <ing.martinezs@outlook.com>
2023-05-29 12:16:48 -0500

    implementación de Az table storage

---
+++ TableStorage/Interface/ITableStorage.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+using Azure.Data.Tables;
+using Azure;
+
+namespace BlobStorage.Interface
+{
+    public interface ITableStorage<T> where T : class, ITableEntity, new()
+    {
+        Task<Response> InsertData(T entity); Task UpdateData(T entity);
+        Task<List<T>> QueryData(Expression<Func<T, bool>> expression);
+    }
+}
---
+++ TableStorage/TableStorage.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq.Expressions;
+using Azure.Data.Tables;
+using Azure;
+using Microsoft.Extensions.Configuration;
+using BlobStorage.Interface;
+namespace BlobStorage
+{
+
+    public class TableStorage<T> : ITableStorage<T> where T : class, ITableEntity, new()
+    {
+        private readonly TableClient _table;
+        private readonly IConfiguration _configuration;
+        public TableStorage(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _table = ConectionStorage().GetAwaiter().GetResult();
+        }
+        public async Task<Response> InsertData(T entity)
+        {
+            return await _table.AddEntityAsync(entity);
+        }
+        public async Task UpdateData(T entity)
+        {
+            await _table.UpsertEntityAsync(entity);
+        }
+        public async Task<List<T>> QueryData(Expression<Func<T, bool>> expression)
+        {
+            List<T> result = new List<T>();
+            AsyncPageable<T> queryResultsMaxPerPage = _table.QueryAsync<T>(expression, maxPerPage: 10);
+            await foreach (var page in queryResultsMaxPerPage.AsPages())
+            {
+                result.AddRange(page.Values);
+            }
+            return result;
+        }
+
+        private async Task<TableClient> ConectionStorage()
+        {
+            var serviceClient = new TableServiceClient(_configuration["Storage:conectionString"]);
+            var tableClient = serviceClient.GetTableClient(typeof(T).Name);
+            return tableClient;
+        }
+    }
+}
